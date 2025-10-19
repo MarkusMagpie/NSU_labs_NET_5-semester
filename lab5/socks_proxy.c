e440fb730a9bd27faee8b2564dfd8403d93057bc
MarkusMagpie <m.sorokin3@g.nsu.ru>
2025-10-19 13:46:43 +0700

    теоритическая подготовка к сдаче lab05

--- lab5/socks_proxy.c
+++ lab5/socks_proxy.c
@@ -30,8 +30,8 @@ typedef struct {
 
 
 void set_nonblock(int fd) {
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(fd, F_GETFL, 0); // беру текущие флаги установленные для дескриптора
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK); // добавляю к установленным флагам O_NONBLOCK
 }
 
 int main(int argc, char *argv[]) {
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
     */
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        perror("[main] socket creation failed");
+        printf("[main] socket creation failed");
         exit(EXIT_FAILURE);
     }
 
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]) {
     // SO_REUSEADDR позволяет переиспользовать порт сразу после завершения сервера. даже если он в состоянии TIME_WAIT
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("[main] setsockopt failed");
+        printf("[main] setsockopt failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
@@ -70,18 +70,18 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_family = AF_INET; // IP адрес к которому будет привязан сокет (IPv4)
-    server_addr.sin_port = htons(port); // номер порта 5000 (в сетевом порядке байт) к которому будет привязан сокет
+    server_addr.sin_port = htons(port); // номер порта (в сетевом порядке байт) к которому будет привязан сокет
 
     // привязка сокета к адресу server_addr
     if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("[main] bind failed");
+        printf("[main] bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     // начало прослушивания (5 потому что в мане прочитал что обычно <= 5)
     if (listen(server_fd, 5) < 0) {
-        perror("[main] listen failed");
+        printf("[main] listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
@@ -107,11 +107,11 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < MAX_CLIENTS; i++) {
             // active clients
             if (clients[i].fd > 0) {
-                // монитор на чтение данные от клиента 
+                // монитор на чтение данных от клиента 
                 FD_SET(clients[i].fd, &temp_read);
                 max_fd = (max_fd > clients[i].fd) ? max_fd : clients[i].fd;
                 
-                // если у клиента есть данные для отправки на целевой сервер, добавляю целевой дескриптор в temp_write для прослушивания на возможность записать  
+                // если у клиента есть данные для отправки на целевой сервер, добавляю целевой дескриптор в temp_write для прослушивания на возможность записать в него
                 if (clients[i].buf_len > 0 && clients[i].target_fd > 0) {
                     FD_SET(clients[i].target_fd, &temp_write);
                     max_fd = (max_fd > clients[i].target_fd) ? max_fd : clients[i].target_fd;
@@ -120,11 +120,11 @@ int main(int argc, char *argv[]) {
             
             // active target servers
             if (clients[i].target_fd > 0) {
-                // монитор на чтение данные от целевого сервера
+                // монитор на чтение данные от целевого сервера клиентом
                 FD_SET(clients[i].target_fd, &temp_read);
                 max_fd = (max_fd > clients[i].target_fd) ? max_fd : clients[i].target_fd;
                 
-                // если есть данные для отправки клиенту, добавляю его дескриптор в temp_write для прослушивания на возможность записать
+                // если есть данные для отправки клиенту, добавляю дескриптор клиента в temp_write для прослушивания на возможность записать в него
                 if (clients[i].buf_len > 0) {
                     FD_SET(clients[i].fd, &temp_write);
                     max_fd = (max_fd > clients[i].fd) ? max_fd : clients[i].fd;
@@ -134,7 +134,7 @@ int main(int argc, char *argv[]) {
 
         int activity = select(max_fd + 1, &temp_read, &temp_write, NULL, NULL);
         if (activity < 0) {
-            perror("select failed");
+            printf("select failed");
             exit(EXIT_FAILURE);
         }
         // temp_read, temp_write модифицированы - выставлены биты только для тех дескрипторов, на которых есть готовность к чтению
@@ -147,7 +147,7 @@ int main(int argc, char *argv[]) {
         if (FD_ISSET(server_fd, &temp_read)) {
             int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
             if (client_fd < 0) {
-                perror("accept failed");
+                printf("accept failed");
                 exit(EXIT_FAILURE);
             }
             
@@ -169,10 +169,10 @@ int main(int argc, char *argv[]) {
                     clients[slot].buf_len = 0;
                     clients[slot].target_fd = 0; // пока нет целевого сервера 
                     
-                    printf("\nновое подключение от %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    printf("\n[main] новое подключение от %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
                 } else {
                     close(client_fd);
-                    printf("\nпревышено максимальное количество клиентов. Клиент %s:%d отключен\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    printf("\n[main] превышено максимальное количество клиентов. Клиент %s:%d отключен\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
                 }
             }
         }
@@ -188,7 +188,7 @@ int main(int argc, char *argv[]) {
                 // ошибка или клиент отключился 
                 if (bytes_read <= 0) {
                     getpeername(clients[i].fd, (struct sockaddr*)&client_addr, &addr_len);
-                    printf("клиент отключен: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    printf("[main] клиент отключен: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
                     // закрытие соединения + очистка элемента массива происходит в обоих случаях
 
                     close(clients[i].fd);
@@ -283,7 +283,7 @@ int main(int argc, char *argv[]) {
                         
                         int target_fd = socket(AF_INET, SOCK_STREAM, 0);
                         if (target_fd < 0) {
-                            perror("[debug] socket creation failed");
+                            printf("[debug] socket creation failed");
                             close(clients[i].fd);
                             clients[i].fd = 0;
                             continue;
@@ -293,7 +293,7 @@ int main(int argc, char *argv[]) {
                         
                         // в неблокирующем режиме connect возвращает EINPROGRESS - соединение начато, но не завершено (в процессе) -> не считаю ошибкой
                         if ((connect(target_fd, (struct sockaddr*)&clients[i].dest_addr, sizeof(clients[i].dest_addr)) < 0) && (errno != EINPROGRESS)) {
-                            perror("[debug] connect failed");
+                            printf("[debug] connect failed");
                             close(target_fd);
                             close(clients[i].fd);
                             clients[i].fd = 0;
@@ -352,7 +352,7 @@ int main(int argc, char *argv[]) {
                         // теперь непосредственно разрешение доменного имени в IPv4 адрес
                         struct hostent *host = gethostbyname(domain); // возвращает структуру hostent с информацией о domain
                         if (!host || !host->h_addr_list[0]) {
-                            perror("[debug] DNS resolution failed");
+                            printf("[debug] DNS resolution failed");
                             char response[] = {SOCKS_VERSION, 0x04}; // domain name not found
 
                             send(clients[i].fd, response, sizeof(response), 0);
@@ -369,7 +369,7 @@ int main(int argc, char *argv[]) {
                         // сокет для целевого сервера
                         int target_fd = socket(AF_INET, SOCK_STREAM, 0);
                         if (target_fd < 0) {
-                            perror("[debug] socket creation failed");
+                            printf("[debug] socket creation failed");
                             close(clients[i].fd);
                             clients[i].fd = 0;
                             continue;
@@ -379,7 +379,7 @@ int main(int argc, char *argv[]) {
 
                         // в неблокирующем режиме connect возвращает EINPROGRESS - соединение начато, но не завершено (в процессе) -> не считаю ошибкой
                         if ((connect(target_fd, (struct sockaddr*)&clients[i].dest_addr, sizeof(clients[i].dest_addr)) < 0) && (errno != EINPROGRESS)) {
-                            perror("[debug] connect failed");
+                            printf("[debug] connect failed");
                             close(target_fd);
                             close(clients[i].fd);
                             clients[i].fd = 0;
@@ -477,6 +477,7 @@ int main(int argc, char *argv[]) {
                     // клиент отправляет данные целевому серверу
                     ssize_t bytes_sent = send(clients[i].target_fd, clients[i].buffer, clients[i].buf_len, 0);
 
+                    // в client_addr хранится имя машины, подключившейся к сокету clients[i].fd
                     getpeername(clients[i].fd, (struct sockaddr*)&client_addr, &addr_len);
                     printf("клиент %s:%d отправил целевому серверу %ld байт\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), bytes_sent);
 
@@ -500,7 +501,7 @@ int main(int argc, char *argv[]) {
                         continue;
                     }
                     
-                    // сдвиг оставшихся данных в буфере
+                    // сдвиг оставшихся данных в буфере на butes_sent байт
                     memmove(clients[i].buffer, clients[i].buffer + bytes_sent, clients[i].buf_len - bytes_sent);
                     clients[i].buf_len -= bytes_sent;
                 }
